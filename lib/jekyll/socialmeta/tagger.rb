59a8ecebe552bc971c9410640b92eb08510421f0
Alex Ibrado <alex@ibrado.org>
2018-01-02 09:25:10 +0800

    Add published/modified time support

--- lib/jekyll/socialmeta/tagger.rb
+++ lib/jekyll/socialmeta/tagger.rb
@@ -18,12 +18,14 @@ module Jekyll
           :title => item.data['title'] || 'Untitled',
           :desc => item.data['description'] || @site_desc,
           :url => @site_url + item.url,
+          :published => item.data['date'] ? item.data['date'].to_datetime.to_s : nil,
+          :modified => File.mtime(screenshot.source[:file]).to_datetime.to_s
         }
 
         @tag_queue << {
           :config => item.data['socialmeta'] || {},
           :info => info,
-          :screenshot => screenshot
+          :screenshot => screenshot,
         }
 
       end
@@ -38,6 +40,9 @@ module Jekyll
           info[:tcl] = screenshot.urls[:tcl]
           info[:tcs] = screenshot.urls[:tcs]
           info[:sizes] = screenshot.sizes
+          info[:source] = screenshot.source[:file]
+
+          info[:alt] = (config['image'] || {})['alt']
 
           file = screenshot.source[:html]
           next if File.extname(file) !~ /\.x?html?/
@@ -55,11 +60,10 @@ module Jekyll
       end
 
       def opengraph(config, info)
-        og_config = (config['opengraph'] || config['facebook'] || {}).
-          merge(@config['opengraph'] || @config['facebook'] || {})
+        og_config = (@config['opengraph'] || @config['facebook'] || {}).
+          merge(config['opengraph'] || config['facebook'] || {})
 
         type = og_config['type'] || 'website';
-        app_id = og_config['app_id'] || og_config['fb_app_id'];
 
         tags = %Q{<meta property="og:url" content="#{info[:url]}"/>\n} +
           %Q{<meta property="og:type" content="#{type}"/>\n} +
@@ -67,18 +71,23 @@ module Jekyll
           %Q{<meta property="og:description" content="#{info[:desc]}"/>\n} +
           %Q{<meta property="og:image" content="#{info[:og]}"/>\n} +
           %Q{<meta property="og:image:width" content="#{info[:sizes][:og][:width]}"/>\n} +
-          %Q{<meta property="og:image:height" content="#{info[:sizes][:og][:height]}"/>\n} +
-          (app_id ? %Q{<meta property="fb:app_id" content="#{app_id}"/>\n} : '')
+          %Q{<meta property="og:image:height" content="#{info[:sizes][:og][:height]}"/>\n}
+
+        tags += %Q{<meta property="og:image:alt" content="#{info[:alt]}"/>\n} if info[:alt]
+
 
         # All others
         og_config.each do |k,v|
-          if k !~ /^(fb[_:]app_id|type|title|description|image)$/
-            if k =~ /:/
-              custom_k = k
-            else
-              custom_k = 'og:' + k
+          if k !~ /^(type|title|description|image|url)/
+            k = 'og:' + k if k !~ /:/
+
+            if v == '$.published'
+              v = info[:published]
+            elsif v == '$.modified'
+              v = info[:modified]
             end
-            tags += %Q{<meta property="#{custom_k}" content="#{v}"/>\n}
+
+            tags += %Q{<meta property="#{k}" content="#{v}"/>\n} if v
           end
         end
 
@@ -87,8 +96,8 @@ module Jekyll
       end
 
       def twittercard(config, info)
-        tc_config = (config['twittercard'] || config['twitter'] || {}).
-          merge(@config['twittercard'] || @config['twitter'] || {})
+        tc_config = (@config['twittercard'] || @config['twitter'] || {}).
+          merge(config['twittercard'] || config['twitter'] || {})
 
         creator = tc_config['creator'] || tc_config['handle'] || tc_config['creator']
         site = tc_config['site'] || tc_config['creator'] || tc_config['handle']
